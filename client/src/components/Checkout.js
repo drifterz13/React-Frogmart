715aa6312ab38c0da6eebc39c04482aa74d47ea3
Mix Mr <totsawat@vertice-international.com>
2018-08-02 14:45:14 +0700

    set up payment

--- client/src/components/Checkout.js
+++ client/src/components/Checkout.js
@@ -1,48 +1,57 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, Component } from 'react'
 import { Link } from 'react-router-dom'
 import NumberFormat from 'react-number-format'
+import PaymentModal from './PaymentModal'
 import '../assets/css/Checkout.css'
 
-const Checkout = ({amount, totalPrice, confirm}) => {
-  return (
-    <div className='row justify-content-center mb-5' style={{flexWrap: 'wrap'}}>
-      <div className='card col-6 checkout-wrapped'>
-        <div className='card-body checkout-body'>
-          <div className='checkout-main mb-3'>
-          <NumberFormat 
-            value={totalPrice} 
-            displayType={'text'} 
-            thousandSeparator={true} 
-            prefix={'THB '} 
-            renderText={value => <div>Subtotal ({amount} items): {value}</div>} 
-            />
-            <p>Shipping: FREE</p>
-          </div>
-          <span className='checkout-main mb-3'>
-            <NumberFormat 
-              value={totalPrice} 
-              displayType={'text'} 
-              thousandSeparator={true} 
-              prefix={'THB '} 
-              renderText={value => (
-                <Fragment>
-                  <h5 className='mr-2'>Total:</h5> <h5 style={{fontStyle: 'italic'}}>{value}</h5>
-                </Fragment>
-              )} />
-          </span>
-          <div className='checkout-button-wrapped'>
-            <Link to='/product' className='btn btn-light mr-3'>Continue shopping</Link>
-            <button 
-              type='button' 
-              className='btn btn-primary'
-              onClick={() => confirm()}
+class Checkout extends Component {
+
+  render() {
+    const { amount, totalPrice, confirm } = this.props
+    return (
+      <div className='row justify-content-center mb-5' style={{ flexWrap: 'wrap' }}>
+        <div className='card col-6 checkout-wrapped'>
+          <div className='card-body checkout-body'>
+            <div className='checkout-main mb-3'>
+              <NumberFormat
+                value={totalPrice}
+                displayType={'text'}
+                thousandSeparator={true}
+                prefix={'THB '}
+                renderText={value => <div>Subtotal ({amount} items): {value}</div>}
+              />
+              <p>Shipping: FREE</p>
+            </div>
+            <span className='checkout-main mb-3'>
+              <NumberFormat
+                value={totalPrice}
+                displayType={'text'}
+                thousandSeparator={true}
+                prefix={'THB '}
+                renderText={value => (
+                  <Fragment>
+                    <h5 className='mr-2'>Total:</h5> <h5 style={{ fontStyle: 'italic' }}>{value}</h5>
+                  </Fragment>
+                )} />
+            </span>
+            <div className='checkout-button-wrapped'>
+              <Link to='/product' className='btn btn-light mr-3'>Continue shopping</Link>
+              <button
+                type='button'
+                data-toggle="modal" 
+                data-target="#exampleModal"
+                className='btn btn-primary'
+                // onClick={() => confirm()}
               >
-              Proceed to checkout</button>
+                Proceed to checkout</button>
+            </div>
           </div>
         </div>
+        <PaymentModal />
       </div>
-    </div>
-  )
+
+    )
+  }
 }
 
 export default Checkout
---
+++ client/src/components/PaymentModal.js
@@ -0,0 +1,129 @@
+import React, { Component } from 'react'
+
+class PaymentModal extends Component {
+  state = {}
+
+  handleSubmit = e => {
+    e.preventDefault()
+    const checkoutForm = document.getElementById('checkout-form')
+    const { Omise } = window
+    Omise.setPublicKey('pkey_test_5csoj7uqs1o8uhko7r6')
+    const cardObject = {
+      name: document.querySelector('[data-name="nameOnCard"]').value,
+      number: document.querySelector('[data-name="cardNumber"]').value,
+      expiration_month: document.querySelector('[data-name="expiryMonth"]').value,
+      expiration_year: document.querySelector('[data-name="expiryYear"]').value,
+      security_code: document.querySelector('[data-name="securityCode"]').value
+    }
+    Omise.createToken('card', cardObject, (statusCode, response) => {
+      if (statusCode === 200) {
+        console.log(response)
+        checkoutForm.omiseToken.value = response.id
+      } else {
+        console.log(response.message)
+      }
+    })
+  }
+
+  render () {
+    return (
+      <div className='modal fade' id='exampleModal' tabIndex='-1'>
+        <div className='modal-dialog' role='document'>
+          <div className='modal-content'>
+            <div className='modal-header' style={{backgroundColor: '#fbfbfb'}}>
+              <h5 className='modal-title' id='exampleModalLabel'>FrogMart Payment</h5>
+              <button type='button' className='close' data-dismiss='modal' aria-label='Close'>
+                <span aria-hidden='true'>&times;</span>
+              </button>
+            </div>
+            <div className='modal-body'>
+              <p>
+                <img src='http://bootstrap-ecommerce.com/main/images/icons/pay-visa.png' alt='visa' />
+                <img className='mx-1' src='http://bootstrap-ecommerce.com/main/images/icons/pay-mastercard.png' alt='mastercard' />
+                <img src='http://bootstrap-ecommerce.com/main/images/icons/pay-american-ex.png' alt='american-ex' />
+              </p>
+              <form id='checkout-form' onSubmit={this.handleSubmit} >
+                <div className='form-group'>
+                  <label>Name on card</label>
+                  <div className='input-group-prepend'>
+                    <span className='input-group-text'>
+                      <i className='fa fa-user'></i>
+                    </span>
+                    <input type='text' data-name='nameOnCard' className='form-control' placeholder='Full Name' />
+                  </div>
+                </div>
+
+                <div className='form-group'>
+                  <label>Card Number</label>
+                  <div className='input-group'>
+                    <div className='input-group-prepend'>
+                      <span className='input-group-text'>
+                        <i className='fa fa-credit-card'></i>
+                      </span>
+                    </div>
+                    <input type='text' data-name='cardNumber' className='form-control' placeholder='••••••••••••••••' />
+                    </div>
+                </div>
+
+                <div className='row'>
+                  <div className='col-sm-8'>
+                    <div className='form-group'>
+                      <label>
+                        <span className='hidden-xs'>Expiration</span>
+                      </label>
+                      <div className='form-inline'>
+                        <select className='form-control' data-name='expiryMonth' style={{width: '45%'}}>
+                          <option value=''>MM</option>
+                          <option value='1'>1</option>
+                          <option value='2'>2</option>
+                          <option value='3'>3</option>
+                          <option value='4'>4</option>
+                          <option value='5'>5</option>
+                          <option value='6'>6</option>
+                          <option value='7'>7</option>
+                          <option value='8'>8</option>
+                          <option value='9'>9</option>
+                          <option value='10'>10</option>
+                          <option value='11'>11</option>
+                          <option value='12'>12</option>
+                        </select>
+                        <span style={{width: '10%', textAlign: 'center'}}> / </span>
+                        <select className='form-control' data-name='expiryYear' style={{ width: '45%' }}>
+                          <option value=''>YYYY</option>
+                          <option value='2018'>2018</option>
+                          <option value='2019'>2019</option>
+                          <option value='2020'>2020</option>
+                          <option value='2021'>2021</option>
+                          <option value='2022'>2022</option>
+                          <option value='2023'>2023</option>
+                          <option value='2024'>2024</option>
+                          <option value='2025'>2025</option>
+                        </select>
+                      </div>
+                    </div>
+                  </div>
+                  <div className='col-sm-4'>
+                    <div className='form-group'>
+                      <label data-toggle='tooltip' title='' data-original-title='3 digits code on back side of the card'>
+                        CVV
+                        <i className='fa fa-question-circle'></i>
+                      </label>
+                      <input type='text' data-name='securityCode' className='form-control' />
+                    </div>
+                  </div>
+                </div>
+
+                <div className='form-group'>
+                  <button className='btn btn-primary'>Checkout</button>
+                </div>
+                <input type='hidden' name='omiseToken' />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default PaymentModal
